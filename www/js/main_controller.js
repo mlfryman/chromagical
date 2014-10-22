89ee3b3f3fa0fac31e95f71f63ba5df0674b3536
Melanie L. Fryman <melanie@frymanet.com>
2014-10-22 11:52:34 -0500

    saving point; I'm working on social sharing

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -2,7 +2,31 @@
   'use strict';
 
   angular.module('chromagic')
-  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+  .controller('MainCtrl', ['$scope', '$interval', '$cordovaSocialSharing', function($scope, $interval, $cordovaSocialSharing){
 
+  $cordovaSocialSharing
+    .shareViaTwitter(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .shareViaWhatsApp(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+
+
+  $cordovaSocialSharing
+    .shareViaFacebook(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
   }]);
 })();
